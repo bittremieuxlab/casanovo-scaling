ca78ece69d0f15bdba939c47526da4ae8d4ea768
vsc20683 vscuser <vsc20683@login58.dodrio.os>
2025-12-16 12:56:44 +0100

    final v2_train_subsets run

--- hpc_scripts/v2_train_subsets/v2_train_subsets.sh
+++ hpc_scripts/v2_train_subsets/v2_train_subsets.sh
@@ -1,8 +1,8 @@
 VAL_FILE="massivekb_data/massiveKB_3cac0386/subsets/val.mgf"
 CONFIG_FILE="hpc_scripts/v2_train_subsets/default.yaml"
 
-for NUM_SPECTRA in 1 2 5 10 20; do
-  for NUM_PEP in 100000 500000 1000000 2000000 3000000 4243254; do
+for NUM_SPECTRA in 20; do
+  for NUM_PEP in 4243254; do
     TRAIN_FILE="massivekb_data/massiveKB_3cac0386/subsets/train_${NUM_SPECTRA}s_${NUM_PEP}p.mgf"
     OUTPUT_DIR="logs/v2_train_subsets/${NUM_SPECTRA}s_${NUM_PEP}p/"
     PBS_LOG_FILE="logs/v2_train_subsets/${NUM_SPECTRA}s_${NUM_PEP}p/pbs_$(date +%y%m%d%H%M%S%4N).out"
